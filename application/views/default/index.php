f44a9e63158acdfe3e5503ba118a95eeb22d4571
Anh Khoa <a8515895@gmail.com>
2018-03-27 17:58:58 +0700

    git

---
+++ application/views/default/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link href="<?=base_url("public/css/bootstrap.min.css")?>" rel="stylesheet">
+        <link href="<?=base_url("public/css/bootstrap-theme.min.css")?>" rel="stylesheet">
+        <link href="<?=base_url("public/plugin/toast/toast.min.css")?>" rel="stylesheet">
+        <link href="<?=base_url("public/css/fontawesome-all.min.css")?>" rel="stylesheet">
+        <link href="<?=base_url("public/plugin/dataTable/dataTable.min.css")?>" rel="stylesheet">
+        <link href="<?=base_url("public/plugin/select2/select2.min.css")?>" rel="stylesheet">
+        <link href="<?=base_url("public/css/reset.css")?>" rel="stylesheet">
+        <link href="<?=base_url("public/css/style.css")?>" rel="stylesheet">
+        
+        <title>Sega Tea</title>
+    </head>
+    <body>
+        
+    </body>
+    <script src="<?=base_url("public/js/jquery-3.3.1.min.js")?>"></script>
+    <script src="<?=base_url("public/js/bootstrap.min.js")?>"></script>
+    <script src="<?=base_url("public/plugin/toast/toast.min.js")?>"></script>
+    <script src="<?=base_url("public/plugin/dataTable/dataTable.min.js")?>"></script>
+    <script src="<?=base_url("public/plugin/select2/select2.min.js")?>"></script>
+
+</html>
+<script>
+    const url = '<?=base_url("admin/")?>';
+</script>
+<script src="<?=base_url("public/js/scriptAdmin.js")?>"></script>
+<script>
+    $(document).ready(function(){
+        $.get(url+'dashboard',{},function(kq){
+            $("#load").hide();
+            $(".wrapper").html(kq)
+        }); 
+      
+        <?php if(!empty($this->session->flashdata())){ echo $this->session->flashdata("login"); } ?>
+    })
+</script>
